68e3d9b3844e5484fa531c1d28261454265ef03c
Smuve-Jeff <smuvejeffpresents@gmail.com>
2026-01-02 13:28:29 -0600

    Delete codeterm directory

--- codeterm/scripts/deb-postinstall.tpl
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Post installation script for .deb package
-
-# Make backend mainSave executable
-chmod +x /opt/CodeTerm/resources/backend/mainSave
--- codeterm/src/ui/utils/circularLoader.tsx
+++
@@ -1,8 +0,0 @@
-import { ImSpinner2 } from 'react-icons/im';
-
-const CircularLoader = () => (
-  <ImSpinner2 className="animate-spin text-[--promptColorGradientEnd] ml-1 mb-1" />
-);
-
-export default CircularLoader;
-
