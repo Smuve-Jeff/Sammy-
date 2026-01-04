0ec6e92c02664b7906aa125332568a21d31c8603
smuvejeffpresents <smuvejeffpresents@gmail.com>
2026-01-04 01:39:20 +0000

    Refactor: Clean up project structure and fix component bindings

---
+++ src/sample-library.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-library',
+  template: '<p>sample-library works!</p>',
+  standalone: true,
+})
+export class SampleLibraryComponent {}
